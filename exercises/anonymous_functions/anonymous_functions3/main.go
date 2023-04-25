909fd5c6400d0bbab9e7a18f4bde14746d3fb68f
Yohannes Kifle <yohannes2kifle@gmail.com>
2023-04-25 19:05:48 +0300

    solutions

--- exercises/anonymous_functions/anonymous_functions3/main.go
+++ exercises/anonymous_functions/anonymous_functions3/main.go
@@ -1,7 +1,6 @@
 // anonymous functions3
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
@@ -16,7 +15,7 @@ func updateStatus() func() string {
 
 	return func() string {
 		index++
-		return "What should I return?"
+		return orderStatus[index]
 	}
 }
 
@@ -27,7 +26,7 @@ func main() {
 	status = anonymous_func()
 	status = anonymous_func()
 
-	if status == "DONE" {
+	if status = anonymous_func(); status == "DONE" {
 		fmt.Println("Good Job!")
 	} else {
 		panic("To complete the challenge the status must be DONE")
--- exercises/structs/structs2/main.go
+++ exercises/structs/structs2/main.go
@@ -1,19 +1,23 @@
 // structs2
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "fmt"
 
+type Phone struct {
+	phone string
+}
+
 type Person struct {
 	// don't just create the phone field here. embed a new struct
+	Phone
 	name string
-	age int
+	age  int
 }
 
 func main() {
 	// contactDetails := ContactDetails{}
-	person := Person{name: "John", age: 32}
+	person := Person{name: "John", age: 32, Phone: Phone{phone: "+01 333 666"}}
 	fmt.Printf("%s is %d years old and his phone is %s\n", person.name, person.age, person.phone)
 }
